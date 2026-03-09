b91edcbc8033c418a0c37f48dac02fa2573bfa74
Rafeu Islam Abid <abidrafeu011@gmail.com>
2026-03-09 19:17:26 +0600

    solved a bug i guess

--- script/login.js
+++ script/login.js
@@ -13,7 +13,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
         // 3-1. true::>> alert>homepage
 alert("Login successful")
 // window.location.replace('/home.html')
-window.location.assign('/home.html')
+window.location.assign('home.html')
     }
     else{
         // 3-2. false::>> alert>return
